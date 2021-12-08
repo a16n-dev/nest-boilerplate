8095f2debc7cc851b8f8bb715548b25319d901fa
alexn400 <alexn400@gmail.com>
2021-12-08 16:09:13 +1300

    get swagger info from package.json

--- src/setup.ts
+++ src/setup.ts
@@ -1,9 +1,9 @@
 import { ValidationPipe, HttpStatus, INestApplication } from '@nestjs/common';
 import { useContainer } from 'class-validator';
-import * as cookieParser from 'cookie-parser';
-import * as session from 'express-session';
-import * as passport from 'passport';
-import * as connectPgSimple from 'connect-pg-simple';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import passport from 'passport';
+import connectPgSimple from 'connect-pg-simple';
 
 import { AppModule } from './app.module';
 
@@ -13,7 +13,7 @@ export function setup(app: INestApplication): INestApplication {
       transform: true,
       whitelist: true,
       errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
-    }),
+    })
   );
 
   app.use(cookieParser(process.env.APP_SECRET));
@@ -33,7 +33,7 @@ export function setup(app: INestApplication): INestApplication {
         sameSite: 'strict',
         secure: process.env.NODE_ENV === 'production',
       },
-    }),
+    })
   );
 
   app.use(passport.initialize());
